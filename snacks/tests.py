933bf92e9a4a1764b28114e35efd28bc15a47a40
Slava Makeev <unclearnuclear@yahoo.com>
2023-07-20 00:22:29 +0000

    finished project

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,62 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Snack
+
+class SnackTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="vaultdweller", email="dweller@email.com", password="pass"
+        )
+        self.snack = Snack.objects.create(
+            title="Nuka-Cola", description="A refreshing drink from the wasteland.", purchaser=self.user,
+        )
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.snack), "Nuka-Cola")
+
+    def test_snack_content(self):
+        self.assertEqual(f"{self.snack.title}", "Nuka-Cola")
+        self.assertEqual(f"{self.snack.purchaser}", "vaultdweller")
+        self.assertEqual(f"{self.snack.description}", "A refreshing drink from the wasteland.")
+
+    def test_snack_list_view(self):
+        response = self.client.get(reverse("snack_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Nuka-Cola")
+        self.assertTemplateUsed(response, "snack_list.html")
+
+    def test_snack_detail_view(self):
+        response = self.client.get(reverse("snack_detail", args=[str(self.snack.id)]))
+        no_response = self.client.get(reverse("snack_detail", args=["100000"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "Purchaser: vaultdweller")
+        self.assertTemplateUsed(response, "snack_detail.html")
+
+    def test_snack_create_view(self):
+        response = self.client.post(
+            reverse("snack_create"),
+            {
+                "title": "Sugar Bombs",
+                "description": "A delicious cereal from before the war.",
+                "purchaser": self.user.id,
+            }, follow=True
+        )
+        new_snack = Snack.objects.get(title="Sugar Bombs")
+        self.assertRedirects(response, reverse("snack_detail", args=[str(new_snack.id)]))
+        self.assertContains(response, new_snack.title)
+        self.assertContains(response, f"Purchaser: {self.user.username}")
+        self.assertContains(response, new_snack.description)
+
+    def test_snack_update_view_redirect(self):
+        response = self.client.post(
+            reverse("snack_update", args=[str(self.snack.id)]),
+            {"title": "Slocum's BuzzBites", "description": "A sweet and caffeinated treat.", "purchaser": self.user.id}
+        )
+        self.assertRedirects(response, reverse("snack_detail", args=[str(self.snack.id)]))
+
+    def test_snack_delete_view(self):
+        response = self.client.get(reverse("snack_delete", args=[str(self.snack.id)]))
+        self.assertEqual(response.status_code, 200)
--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7wcu2bc#ww74l3=#wqovt246stnar2p32fx13#__0#xwi_*3mn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'snacks',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'snacks_crud_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'templates/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
